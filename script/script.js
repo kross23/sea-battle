2f160a0e2d342371a5f98619e454b841d5d3fb4a
kross <662242@mail.ru>
2020-03-06 23:39:03 +0500

    day 3

--- script/script.js
+++ script/script.js
@@ -7,30 +7,83 @@ const again = document.getElementById('again');
 const heder = document.querySelector('.header');
 
 const game={
-ships: [
-    {
-location:['26','36','46','56'],
-     hit:['','','',''],
+ships: [],
+shipCount : 0,
+optionShip : {
+count : [1, 2, 3, 4],
+size : [4, 2, 3, 1],
+},
+collizion : new Set(),
+ generateShip(){
+   for(let i=0; i < this.optionShip.count.length ; i++){
+
+        for(let j=0;j<this.optionShip.count[i];j++){
+         const size = this.optionShip.size[i];
+         const ship = this.generateOptionShip(size);
+            this.ships.push(ship);
+            this.shipCount++;
+        }
+   }
+ } ,
+ generateOptionShip(shipSize){
+      const ship = {
+        hit:[],
+        location:[],
+      };
+      const direction = Math.random() < 0.5;
+      let x, y;
+      if(direction){
+        x=Math.floor(Math.random()*10);
+        y=Math.floor(Math.random()*(10 - shipSize));
+      }
+      else{
+        x=Math.floor(Math.random()*(10 - shipSize));
+        y=Math.floor(Math.random()*10);
+      }
+
+      for(let i=0;i< shipSize ;i++){
+          if(direction){
+            ship.location.push(x+''+(y+i));
+          }else{
+            ship.location.push((x+i)+''+y);
+          }
+          ship.hit.push('');
+      }
+
+      if(this.checKollizium(ship.location)){
+          return this.generateOptionShip(shipSize);
+      }
+      this.addCollizions(ship.location);
+      return ship;
     },
-    {
-location:['11','12','13'],
-     hit:['','',''],
+    checKollizium(location){
+        for(const coord of location){
+            if(this.collizion.has(coord)){
+                return true;
+            }
+        }
     },
-    {
-location:['31','32'],
-     hit:['',''],
-    },
-    {
-location:['38'],
-    hit:[''], 
+    addCollizions(location){
+        for(let i = 0; i<location.length;i++){
+            const startCordX = location[i][0]-1;
+            for(let j= startCordX;j< startCordX+3  ;j++){
+                const startCordY = location[i][1]-1;
+                for(let z=startCordY;z<startCordY+3;z++){
+                    if(j>=0 && j<10 && z >= 0 && z < 10){
+                    const coord = j+''+z;
+                   
+                        this.collizion.add(coord);
+                        
+                    }
+                }
+            }
+        }
     }
-     ],
-     shipCount : 4,
 };
 
 
 const play = {
-    record : localStorage.getItem('eaBatlRecord') || 0,
+    record : localStorage.getItem('seaBatlRecord') || 0,
     shot : 0 ,
     hit : 0 ,
     dead : 0,
@@ -61,11 +114,11 @@ const show = {
 
     }
 }
-let gamover = false;
+
 const fire = (event) =>{
     const target = event.target;
     
-    if(target.classList.length < 0 || target.tagName !=='TD' || gamover === true )return;
+    if(target.classList.length < 0 || target.tagName !=='TD' || !game.shipCount )return;
     show.miss(target);
     play.updateDate='shot';
     
@@ -83,10 +136,10 @@ const fire = (event) =>{
                     show.dead(document.getElementById(id));
                 }
                 game.shipCount-- ;
-                if(game.shipCount < 1){
+                if(!game.shipCount){
                     heder.textContent='Игра окончена !!';
                     heder.style.color = 'red';
-                    gamover = true;
+                 
                     if(play.record > play.shot || play.record === 0 ){
                         localStorage.setItem('seaBatlRecord', play.shot);
                         play.record = play.shot;
@@ -102,10 +155,17 @@ const fire = (event) =>{
 const init = () => {
 
     enemy.addEventListener('click',fire);
+    game.generateShip();
+    console.log(game);
     play.render();
     again.addEventListener('click', ()=>{
         location.reload();
     })
+    record.addEventListener('dblclick',()=>{
+        localStorage.clear();
+        play.record = 0;
+        play.render();
+    });
 };
 
 
